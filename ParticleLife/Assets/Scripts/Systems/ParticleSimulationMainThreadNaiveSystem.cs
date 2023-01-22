42a94d4abafe3bf76fef987e0926a36f5ed43a95
Vakey Rujevic <vakey.rujevic@gmail.com>
2023-01-22 17:48:44 -0500

    Update ParticleSimulationMainThreadNaiveSystem.cs

--- ParticleLife/Assets/Scripts/Systems/ParticleSimulationMainThreadNaiveSystem.cs
+++ ParticleLife/Assets/Scripts/Systems/ParticleSimulationMainThreadNaiveSystem.cs
@@ -2,7 +2,6 @@ using Unity.Burst;
 using Unity.Entities;
 using Unity.Collections;
 using Unity.Transforms;
-using Unity.Mathematics;
 
 [BurstCompile]
 public partial struct ParticleSimulationMainThreadNaiveSystem: ISystem
@@ -20,47 +19,19 @@ public partial struct ParticleSimulationMainThreadNaiveSystem: ISystem
         var particlesQuery = SystemAPI.QueryBuilder().WithAll<ParticleTag, Velocity, WorldTransform>().Build();
 
         var particleTags = particlesQuery.ToComponentDataArray<ParticleTag>(Allocator.TempJob);
-        var velocities = particlesQuery.ToComponentDataArray<Velocity>(Allocator.TempJob);
         var entities = particlesQuery.ToEntityArray(Allocator.TempJob);
         var transforms = particlesQuery.ToComponentDataArray<WorldTransform>(Allocator.TempJob);
 
-        for (var i = 0; i < transforms.Length; i++)
-        {
-            var aColor = (int)particleTags[i].color;
-            var aPos = transforms[i].Position;
-            var aVel = velocities[i].value;
-            var entity = entities[i];
-            var force = float3.zero;
-
-            for (var k = 0; k < transforms.Length; k++)
-            {
-                var bColor = (int)particleTags[k].color;
-                var bPos = transforms[k].Position;
-                var bEntity = entities[k];
-                
-                if (entity != bEntity) {
-                    var delta = spawner.GetDelta(aPos, bPos);
-                    var distance = math.sqrt(delta.x * delta.x + delta.y * delta.y);
-                    
-                    if (distance < spawner.particleProperties.minRadius)
-                    {
-                        var attraction = spawner.particleProperties.innerDetract;
-                        force += spawner.GetForce(attraction, distance, delta);
-                    }
-                    else if (distance < spawner.particleProperties.maxRadius)
-                    {
-                        var attraction = particleRuleBuffer[aColor * 2 + bColor].attraction;
-                        force += spawner.GetForce(attraction, distance, delta);
-                    }
-                }
-            }
-
-            aVel = math.lerp(aVel, force * 0.4f, 0.5f);
-            commandBuffer.SetComponent<Velocity>(entity, new Velocity { value = aVel });
-        }
+        new ParticleSimulationNaiveJob{ 
+            spawner = spawner,
+            particleRuleBuffer = particleRuleBuffer,
+            particleTags = particleTags,
+            entities = entities,
+            transforms = transforms,
+            commandBuffer = commandBuffer.AsParallelWriter(),
+        }.Run();
 
         particleTags.Dispose();
-        velocities.Dispose();
         entities.Dispose();
         transforms.Dispose();
     }
